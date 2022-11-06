b67a65ec3888a6406195a7ed77dd95c127570aa8
amine <m.amine.osm@gmail.com>
2022-11-06 01:04:31 +0100

    Initial commit implement Header + about

---
+++ src/container/Work/Work.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import './Work.scss';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'react'
+const Work = () => {
+  return (
+    <div>Work</div>
+  )
+}
+
+export default Work;
